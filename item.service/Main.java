1b453779504810d32e1f69dd230265a62f5b7e1d
sjh010 <sjh010@naver.com>
2014-03-11 21:57:45 +0900

    Create Main.java

---
+++ item.service/Main.java
@@ -0,0 +1,14 @@
+package item.service;
+
+import item.view.ItemUI;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ItemUI obj = new ItemUI();
+		
+		obj.intro();	
+	}
+}
